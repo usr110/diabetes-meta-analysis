7dddecb2aafa6af2cc39317140eb76728bc23a64
usr110 <m.aliabbas@gmail.com>
2016-03-15 15:46:32 +0000

    Use nvd3 library instead. Also introduce logic for the data table.

--- server.R
+++ server.R
@@ -3,15 +3,36 @@ shinyServer(function(input, output, session){
   
   output$plotMetaAnalysis <- renderChart({
     filterScData()
-    # "dose","RR","95Percent-CI-Lower-bound","95Percent-CI-Higher-bound"
-    # "dose","RR","Lower Band with 95% CI","Higher Band with 95% CI"
-    h1 <- mPlot(x = "dose", 
-                y = c("Higher Band with 95% CI", "RR", "Lower Band with 95% CI"), 
-                data = scData, type = "Line")
-    h1$set(pointSize = 0)
-    h1$set(hideHover = "auto")
+#     # "dose","RR","95Percent-CI-Lower-bound","95Percent-CI-Higher-bound"
+#     # "dose","RR","Lower Band with 95% CI","Higher Band with 95% CI"
+#     h1 <- mPlot(x = "dose", 
+#                 y = c("Higher Band with 95% CI", "RR", "Lower Band with 95% CI"), 
+#                 data = scData, type = "Line")
+#     h1$set(pointSize = 0)
+#     h1$set(hideHover = "auto")
+#     h1$set(dom = "plotMetaAnalysis")
+#     return (h1)
+    
+    
+    require(rCharts)
+    options(stringsAsFactors = F)
+    library(reshape2)
+    melted_data = melt(scData, id = "dose")
+    library(rCharts)
+    # NVD3
+    h1 <- nPlot(value ~ dose, group = 'variable', data = melted_data, 
+                type = c('lineChart'))
+    
+    
+                # type = 'lineChart')
+    # h1$chart(width=1200, height=500, tooltipContent = "#! function(key, x, y, e)")
+    h1$xAxis(axisLabel = 'Lesiure Time Physical Activity (MET hours per week)')
+    h1$yAxis(axisLabel = "RR", width = 30, height = 500)
+    h1$yAxis(tickFormat = "#! function(d) {return d3.round(d, 2);} !#")
+    # h1$chart(margin=list(left= 30, right = 30)) 
+    h1$chart(useInteractiveGuideline = TRUE)
     h1$set(dom = "plotMetaAnalysis")
-    return (h1)
+    return(h1)
   })
   
   filterScData <- reactive({
@@ -27,4 +48,16 @@ shinyServer(function(input, output, session){
     }
     scData <<- data
   })
+  
+  output$summaryDT <- DT::renderDataTable({
+    if(is.null(summary_table)){
+      return()
+    }
+    
+    DT::datatable(summary_table, options = list(pageLength = 10), colnames = names(summary_table)) 
+#     %>%
+#       formatRound(columns = names(numericZoneColNames), digits=2)
+  })
+  
+  shinyjs::onclick("help", shinyjs::toggle(id = "helpText", anim = FALSE))
 })
