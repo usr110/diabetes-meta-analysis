2127971d8248d504c84f6d6d2170ae2f31f92841
usr110 <m.aliabbas@gmail.com>
2016-03-31 18:21:18 +0100

    Disable installation of packages

--- setup.R
+++ setup.R
@@ -15,10 +15,10 @@ pkgs <- c(
 # lapply(pkgs, library, character.only = T)
 reqs <- as.numeric(lapply(pkgs, require, character.only = TRUE))
 # Install packages we require
-if(sum(!reqs) > 0) install.packages(pkgs[!reqs])
+#if(sum(!reqs) > 0) install.packages(pkgs[!reqs])
 
-if(!require(rCharts)) devtools::install_github("rCharts", "ramnathv")
+#if(!require(rCharts)) devtools::install_github("rCharts", "ramnathv")
 
-if(!require(shiny)) devtools::install_github("shiny", "rstudio")
+#if(!require(shiny)) devtools::install_github("shiny", "rstudio")
 
 source("data-processing.R")
