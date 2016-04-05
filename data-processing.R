6ef3d91383b366f86c0f99b94439468c841823a6
usr110 <m.aliabbas@gmail.com>
2016-04-05 15:56:37 +0100

    Read MMET data

--- data-processing.R
+++ data-processing.R
@@ -1,6 +1,7 @@
 # Read an xslx file and convert it as a csv
 # write.csv(readxl::read_excel(file.choose()), "data/scenario-A.csv", row.names = F)
 scA <- read.csv("data/scenario-A.csv", as.is = T, check.names=FALSE)
+mmet <- read.csv("data/MMET.csv", as.is = T, check.names=FALSE)
 #smooth.spline(scA$dose, scA$RR, spar=1)
 scB <- read.csv("data/scenario-B.csv", as.is = T, check.names=FALSE)
 scC <- read.csv("data/scenario-C.csv", as.is = T, check.names=FALSE)
