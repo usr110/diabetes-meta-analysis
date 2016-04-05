ddf7cce23546637e62a17573981dce9fd8ff455c
usr110 <m.aliabbas@gmail.com>
2016-04-05 16:22:52 +0100

    Remove unused code

--- server.R
+++ server.R
@@ -61,116 +61,12 @@ shinyServer(function(input, output, session){
     p
   }
   
-  #   output$plotScenarioA <- renderChart({
-  #     
-  #     require(rCharts)
-  #     options(stringsAsFactors = F)
-  #     library(reshape2)
-  #     melted_data = melt(scA, id = "dose")
-  #     library(rCharts)
-  #     h1 <- hPlot(value ~ dose, group = 'variable', data = melted_data, 
-  #                 type = 'line')
-  #     # h1$yAxis(overrideMax = 1, overrideMin = 0)
-  #     h1$xAxis(axisLabel = 'Scenario B: Leisure Time Physical Activity (MET hours per week)')
-  #     h1$yAxis(axisLabel = "RR", width = 30, height = 500)
-  #     h1$yAxis(tickFormat = "#! function(d) {return d3.round(d, 2);} !#")
-  #     h1$tooltip(shared = T)
-  #     h1$exporting(enabled = T)
-  #     h1$set(dom = "plotScenarioA")
-  #     return(h1)
-  #     
-  #   })
-  #   
-  #   
-  #   
-  #   output$plotScenarioB <- renderChart({
-  #     require(rCharts)
-  #     options(stringsAsFactors = F)
-  #     library(reshape2)
-  #     melted_data = melt(scB, id = "dose")
-  #     library(rCharts)
-  #     h1 <- hPlot(value ~ dose, group = 'variable', data = melted_data, 
-  #                 type = 'line')
-  #     # h1$yAxis(overrideMax = 1, overrideMin = 0)
-  #     h1$xAxis(axisLabel = 'Scenario B: Leisure Time Physical Activity (MET hours per week)')
-  #     h1$yAxis(axisLabel = "RR", width = 30, height = 500)
-  #     h1$yAxis(tickFormat = "#! function(d) {return d3.round(d, 2);} !#")
-  #     h1$tooltip(shared = T)
-  #     h1$exporting(enabled = T)
-  #     h1$set(dom = "plotScenarioB")
-  #     return(h1)
-  #   })
-  #   
-  #   output$plotScenarioC <- renderChart({
-  #     require(rCharts)
-  #     options(stringsAsFactors = F)
-  #     library(reshape2)
-  #     melted_data = melt(scC, id = "dose")
-  #     library(rCharts)
-  #     h1 <- hPlot(value ~ dose, group = 'variable', data = melted_data, 
-  #                 type = 'line')
-  #     h1$xAxis(axisLabel = 'Scenario C: Leisure Time Physical Activity (MET hours per week)')
-  #     h1$yAxis(axisLabel = "RR", width = 30, height = 500)
-  #     h1$yAxis(tickFormat = "#! function(d) {return d3.round(d, 2);} !#")
-  #     h1$tooltip(shared = T)
-  #     h1$exporting(enabled = T)
-  #     h1$set(dom = "plotScenarioC")
-  #     return(h1)
-  #   })
-  #   
-  #   output$plotScenarioD <- renderChart({
-  #     require(rCharts)
-  #     options(stringsAsFactors = F)
-  #     library(reshape2)
-  #     melted_data = melt(scD, id = "dose")
-  #     library(rCharts)
-  #     h1 <- hPlot(value ~ dose, group = 'variable', data = melted_data, 
-  #                 type = 'line')
-  #     h1$xAxis(axisLabel = 'Scenario D: Leisure Time Physical Activity (MET hours per week)')
-  #     h1$yAxis(axisLabel = "RR", width = 30, height = 500)
-  #     h1$yAxis(tickFormat = "#! function(d) {return d3.round(d, 2);} !#")
-  #     h1$tooltip(shared = T)
-  #     h1$exporting(enabled = T)
-  #     h1$set(dom = "plotScenarioD")
-  #     return(h1)
-  #   })
-  
-  
-  filterScData <- reactive({
-    data <- NULL
-    if (input$inSc == 'Scenario A'){
-      data <- scA
-    }else if(input$inSc == 'Scenario B'){
-      data <- scB
-    }else if(input$inSc == 'Scenario C'){
-      data <- scC
-    }else if(input$inSc == 'Scenario D'){
-      data <- scD
-    }
-    # smooth data
-    
-    #     data1 <- smooth.spline(data$dose, data$RR, spar=1, keep.data = TRUE)
-    #     data2 <- smooth.spline(data[,3], data$RR, spar=1, keep.data = TRUE)
-    #     data3 <- smooth.spline(data[,4], data$RR, spar=1, keep.data = TRUE)
-    
-    # cat(length(data1$x), ' : ', length(data2$x), ' : ', length(data3$x), "\n")
-    
-    # "dose","RR","Lower Band with 95% CI","Higher Band with 95% CI"
-    #     data$dose <- data1$x
-    #     data$RR <- data1$y
-    #     data[["Lower Band with 95% CI"]] <- data2$x
-    #     data[["Higher Band with 95% CI"]] <- data3$x
-    scData <<- data
-  })
-  
   output$summaryDT <- DT::renderDataTable({
     if(is.null(summary_table)){
       return()
     }
     
     DT::datatable(summary_table, options = list(pageLength = 10), colnames = names(summary_table)) 
-    #     %>%
-    #       formatRound(columns = names(numericZoneColNames), digits=2)
   })
   
   shinyjs::onclick("help", shinyjs::toggle(id = "helpText", anim = F))
