fd67c0961d4a6325dfd453787ea3dd8ec0a0d049
usr110 <m.aliabbas@gmail.com>
2016-04-14 14:44:31 +0100

    Replace help text with a markdown document

--- ui.R
+++ ui.R
@@ -26,7 +26,7 @@ shinyUI(fluidPage(
   fluidRow(
     column(width = 5, offset = 5,
            downloadButton('downloadMMETData', 'Download Marginal MET Data'))
-           
+    
   ),
   fluidRow(
     column(width = 5, offset= 1,
@@ -85,9 +85,9 @@ shinyUI(fluidPage(
                                     <li>Session duration: 45 minutes</li>
                                     <li>Intensity: MVPA = 3.5 MET/h; LPA= 2 MET/h; VPA= 7 MET/h</li>
                                     </ul>"))
-                      )))
-                      )
-                      )
+                    )))
+             )
+           )
     ),
     column(width = 5,
            plotlyOutput("plotScenarioD"),
@@ -123,22 +123,9 @@ shinyUI(fluidPage(
            (div(
              id = "helpText",
              DT::dataTableOutput("summaryDT"),
-             helpText(HTML("
-                           LTPA converted to MET.hr per week with results pooled in a two stage 
-                           random effects model. RRs were derived from a common lowest physical activity category 
-                           within each study. Listed exposure levels (as indicated by the dashed lines) 
-                           were chosen to represent meaningful and easy to interpret PA volumes equivalent to 
-                           the following: 30 min of MVPA; 1 hour of MVPA; Rounded value to allow for comparison with 
-                           Generalized Least Square linear PA exposure increments; 150 minutes of PA/current recommended 
-                           guidelines; double the recommended guidelines and two high PA exposure levels investigating the 
-                           risk reductions at the higher end of the LTPA spectrum. The bold line indicates the pooled 
-                           restricted cubic spline model and the grey area indicates the 95% confidence intervals of the pooled curve. Duration 
-                           assumptions were necessary in 9 out of 27 observation (applied as 45 min/session in Scenarios A and C; and 30 min/session 
-                           in Scenarios B and D), and intensity assumptions were necessary in 15 out of 27 observations (applied as LPA=3 MET, 
-                           MVPA=4.5MET, and VPA=8 MET in Scenarios A and B; and LPA=2 MET, MVPA=3.5MET, and VPA=7 MET in Scenarios C and D)."
-                           ))
-             
-           ))
+             includeMarkdown("help.md")
+           )
+           )
     )
   )
 ))
